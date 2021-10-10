4ad7c5485d85a6527f98ca81aa20b05de63d171d
hrwx <himanshuwarekar@yahoo.com>
2021-10-10 13:44:18 +0100

    fix: calculate billing and costing

--- jira/tasks/daily.py
+++ jira/tasks/daily.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.utils import now_datetime, get_datetime_str
+from frappe.utils import now_datetime, get_datetime_str, flt
 from jira.jira_client import JiraClient
 
 
@@ -18,7 +18,7 @@ def pull_issues_from_jira():
 			issue = jira_client.get_issue(issue.get("id"))
 			project = issue.get("fields", {}).get("project", {}).get("name")
 
-			worklogs = jira_client.get_timelogs_by_issue(issue.get("id"))
+			worklogs = jira_client.get_timelogs_by_issue(issue.get("id"), started_after=jira_settings.last_synced_on)
 
 			for worklog in worklogs.get("worklogs", []):
 				if not logs.get(f"{project}::{user_details.user}", None):
@@ -62,24 +62,33 @@ def create_timesheets(jira_settings, worklogs):
 		user = details[1]
 
 		employee = frappe.db.get_value("Employee", {"user_id": user}) or user
-		timesheet = {
-			"doctype" :"Timesheet",
-			"employee": employee,
-			"time_logs": []
-		}
+		timesheet = frappe.new_doc("Timesheet")
+		timesheet.employee = employee
 
 		for log in worklogs[worklog]:
-			timesheet.get("time_logs").append({
-				"doctype": "Timesheet Detail",
+			base_billing_rate =  flt(project_map.get(project, {}).get("billing_rate", 0)) * timesheet.exchange_rate
+			base_costing_rate =  flt(user_cost_map.get(user, 0)) * timesheet.exchange_rate
+
+			billing_rate =  project_map.get(project, {}).get("billing_rate", 0)
+			costing_rate =  user_cost_map.get(user, 0)
+
+			billing_hours  =  log.get("timeSpentSeconds", 0) / 3600
+
+			timesheet.append("time_logs", {
 				"from_time": get_datetime_str(log.get("started")),
-				"hours": log.get("timeSpentSeconds", 0) / 3600,
-				"project": project_map.get(project, {}).get("erpnext_project", None) or project,
+				"hours": billing_hours,
+				"project": project_map.get(project, {}).get("erpnext_project", project),
 				"is_billable": True,
-				"billing_hours": log.get("timeSpentSeconds", 0) / 3600,
-				"billing_rate": project_map.get(project, {}).get("billing_rate", None),
-				"costing_rate": user_cost_map.get(user, None),
+				"billing_hours": billing_hours,
+				"base_billing_rate": base_billing_rate,
+				"billing_rate": billing_rate,
+				"base_costing_rate": base_costing_rate,
+				"costing_rate": costing_rate,
+				"base_billing_amount": flt(billing_hours) * flt(base_billing_rate),
+				"billing_amount": flt(billing_hours) * flt(billing_rate),
+				"costing_amount": flt(costing_rate) * flt(billing_hours),
 				"jira_issue": log.get("issueId"),
 				"jira_worklog": log.get("id")
 			})
 
-		frappe.get_doc(timesheet).insert(ignore_mandatory=True, ignore_links=True, ignore_permissions=True)
+		timesheet.insert(ignore_mandatory=True, ignore_links=True, ignore_permissions=True)
