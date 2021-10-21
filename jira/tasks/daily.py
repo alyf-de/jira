a82a9dc916f73c5125f39559f7cb8bb64cf37d72
barredterra <14891507+barredterra@users.noreply.github.com>
2021-10-21 15:48:22 +0200

    refactor: format with black

--- jira/tasks/daily.py
+++ jira/tasks/daily.py
@@ -5,15 +5,16 @@ import frappe
 from frappe.utils import now_datetime, get_datetime_str, flt, get_date_str
 from jira.jira_client import JiraClient
 
+
 @frappe.whitelist()
 def pull_issues_from_jira(project=None):
 	"""
-		Creates a dictionary with date as key and corresponding logs
-		{
+	Creates a dictionary with date as key and corresponding logs
+	{
 			"date": {
-				"project_key + email_address + account_id": [worklogs]
+					"project_key + email_address + account_id": [worklogs]
 			}
-		}
+	}
 	"""
 	filters = {"enabled": 1}
 	if project:
@@ -22,7 +23,11 @@ def pull_issues_from_jira(project=None):
 	for jira in frappe.get_all("Jira Settings", filters=filters):
 		jira_settings = frappe.get_doc("Jira Settings", jira.name)
 		user_list = [user.email for user in jira_settings.billing]
-		jira_client = JiraClient(jira_settings.url, jira_settings.api_user, jira_settings.get_password(fieldname="api_key"))
+		jira_client = JiraClient(
+			jira_settings.url,
+			jira_settings.api_user,
+			jira_settings.get_password(fieldname="api_key"),
+		)
 		logs = {}
 
 		for mapping in jira_settings.mappings:
@@ -30,28 +35,41 @@ def pull_issues_from_jira(project=None):
 
 			for issue in issues.get("issues", []):
 				_issue = jira_client.get_issue(issue.get("id"))
-				worklogs = jira_client.get_timelogs_by_issue(issue.get("id"), started_after=jira_settings.last_synced_on)
+				worklogs = jira_client.get_timelogs_by_issue(
+					issue.get("id"), started_after=jira_settings.last_synced_on
+				)
 
 				for worklog in worklogs.get("worklogs", []):
 					date = get_date_str(worklog.get("started"))
 					email_address = worklog.get("author", {}).get("emailAddress", None)
-					if not email_address in user_list:
+					if email_address not in user_list:
 						continue
 
-					jira_user_account_id = worklog.get("author", {}).get("accountId", None)
-					worklog.update({
-						"_issueKey": _issue.get('key'),
-						"issueURL": f"{jira_settings.url}/browse/{_issue.get('key')}",
-						"issueDescription": _issue.get("fields", {}).get("summary")
-					})
+					jira_user_account_id = worklog.get("author", {}).get(
+						"accountId", None
+					)
+					worklog.update(
+						{
+							"_issueKey": _issue.get("key"),
+							"issueURL": f"{jira_settings.url}/browse/{_issue.get('key')}",
+							"issueDescription": _issue.get("fields", {}).get("summary"),
+						}
+					)
 
 					if not logs.get(date):
 						logs[date] = {}
 
-					if not logs.get(date).get(f"{mapping.jira_project_key}::{email_address}::{jira_user_account_id}", None):
-						logs[date][f"{mapping.jira_project_key}::{email_address}::{jira_user_account_id}"] = []
+					if not logs.get(date).get(
+						f"{mapping.jira_project_key}::{email_address}::{jira_user_account_id}",
+						None,
+					):
+						logs[date][
+							f"{mapping.jira_project_key}::{email_address}::{jira_user_account_id}"
+						] = []
 
-					logs[date][f"{mapping.jira_project_key}::{email_address}::{jira_user_account_id}"].append(worklog)
+					logs[date][
+						f"{mapping.jira_project_key}::{email_address}::{jira_user_account_id}"
+					].append(worklog)
 
 		create_timesheets(jira_settings, logs)
 		jira_settings.last_synced_on = now_datetime()
@@ -64,7 +82,7 @@ def get_project_map(jira_settings):
 	for project in jira_settings.mappings:
 		project_map[project.jira_project_key] = {
 			"erpnext_project": project.erpnext_project,
-			"billing_rate": project.billing_rate
+			"billing_rate": project.billing_rate,
 		}
 
 	return project_map
@@ -94,8 +112,13 @@ def create_timesheets(jira_settings, worklogs):
 			timesheet.jira_user_account_id = jira_user_account_id
 
 			for log in worklogs[worklog_date][worklog]:
-				base_billing_rate = flt(project_map.get(project, {}).get("billing_rate", 0)) * timesheet.exchange_rate
-				base_costing_rate = flt(user_cost_map.get(user, 0)) * timesheet.exchange_rate
+				base_billing_rate = (
+					flt(project_map.get(project, {}).get("billing_rate", 0))
+					* timesheet.exchange_rate
+				)
+				base_costing_rate = (
+					flt(user_cost_map.get(user, 0)) * timesheet.exchange_rate
+				)
 
 				billing_rate = project_map.get(project, {}).get("billing_rate", 0)
 				costing_rate = user_cost_map.get(user, 0)
@@ -107,23 +130,31 @@ def create_timesheets(jira_settings, worklogs):
 					for comm in comment.get("content", []):
 						description += comm.get("text", "") + " "
 
-				timesheet.append("time_logs", {
-					"from_time": get_datetime_str(log.get("started")),
-					"hours": billing_hours,
-					"project": project_map.get(project, {}).get("erpnext_project", project),
-					"is_billable": True,
-					"description": description,
-					"billing_hours": billing_hours,
-					"base_billing_rate": base_billing_rate,
-					"billing_rate": billing_rate,
-					"base_costing_rate": base_costing_rate,
-					"costing_rate": costing_rate,
-					"base_billing_amount": flt(billing_hours) * flt(base_billing_rate),
-					"billing_amount": flt(billing_hours) * flt(billing_rate),
-					"costing_amount": flt(costing_rate) * flt(billing_hours),
-					"jira_issue": log.get("issueId"),
-					"jira_issue_url": log.get("issueURL"),
-					"jira_worklog": log.get("id")
-				})
+				timesheet.append(
+					"time_logs",
+					{
+						"from_time": get_datetime_str(log.get("started")),
+						"hours": billing_hours,
+						"project": project_map.get(project, {}).get(
+							"erpnext_project", project
+						),
+						"is_billable": True,
+						"description": description,
+						"billing_hours": billing_hours,
+						"base_billing_rate": base_billing_rate,
+						"billing_rate": billing_rate,
+						"base_costing_rate": base_costing_rate,
+						"costing_rate": costing_rate,
+						"base_billing_amount": flt(billing_hours)
+						* flt(base_billing_rate),
+						"billing_amount": flt(billing_hours) * flt(billing_rate),
+						"costing_amount": flt(costing_rate) * flt(billing_hours),
+						"jira_issue": log.get("issueId"),
+						"jira_issue_url": log.get("issueURL"),
+						"jira_worklog": log.get("id"),
+					},
+				)
 
-			timesheet.insert(ignore_mandatory=True, ignore_links=True, ignore_permissions=True)
+			timesheet.insert(
+				ignore_mandatory=True, ignore_links=True, ignore_permissions=True
+			)
