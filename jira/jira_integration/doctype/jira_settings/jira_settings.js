e502550517cc783970270db22e7dce76904059c5
barredterra <14891507+barredterra@users.noreply.github.com>
2022-12-12 16:40:45 +0100

    feat: enqueue jira sync

--- jira/jira_integration/doctype/jira_settings/jira_settings.js
+++ jira/jira_integration/doctype/jira_settings/jira_settings.js
@@ -1,21 +1,17 @@
 // Copyright (c) 2021, Alyf GmbH and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Jira Settings', {
+frappe.ui.form.on("Jira Settings", {
 	refresh: function (frm) {
 		if (!frm.is_new() && frm.doc.enabled) {
-			frm.add_custom_button(__('Sync'), function () {
-				frappe.show_alert({ message: __("Syncing"), indicator: 'blue' });
-				frappe.call({
-					method: "jira.tasks.daily.sync_work_logs_from_jira",
-					args: {
-						jira_settings_name: frm.doc.name
-					},
-					callback: function (r) {
-						frappe.show_alert({ message: __("Synced"), indicator: 'green' });
-					}
+			frm.add_custom_button(__("Sync"), function () {
+				frm.call("start_snyc", {}, (r) => {
+					frappe.show_alert({
+						message: __("Sync enqueued"),
+						indicator: "green",
+					});
 				});
 			});
 		}
-	}
+	},
 });
